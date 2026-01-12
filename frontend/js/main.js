12633ad3f924da5b95579ffa3520923dadc8f639
Hubaldium <jake.mieth@icloud.com>
2026-01-12 21:09:09 +0100

    Adding Functionality

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,9 @@
-import { state, saveState } from "/state.js";
+import { state, saveState } from "./state.js";
 
 function updateUI() {
     const moneyDisplay = document.getElementById("stat-value-money");
     const workersDisplay = document.getElementById("stat-value-workers");
+
 
     if (moneyDisplay) {
         moneyDisplay.textContent = (state.bank_account + state.bank_account_savings).toLocaleString() + " $";
